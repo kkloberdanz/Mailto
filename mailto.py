d1cba0eb552376f416afa1add15fa5be126a4d95
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-10-03 00:37:27 -0500

    starts mail service after getting info

--- mailto.py
+++ mailto.py
@@ -14,31 +14,34 @@ for i in range(1, len(sys.argv)):
         message = sys.argv[i+1]
     elif item == "--from":
         sender_email_address = sys.argv[i+1]
-        pw = getpass.getpass()
     elif item == "--to":
         reciever_email_address = sys.argv[i+1]
 
-mail = smtplib.SMTP('smtp.gmail.com', 587) 
-mail.ehlo() 
-mail.starttls()
 
 if not sender_email_address:
     sender_email_address = input("Sender's email address > ") 
-    pw = getpass.getpass()
 
-if not sender_email_address:
-    print(sys.argv[0], "error: requires sender's email address")
+    if not sender_email_address:
+        print(sys.argv[0], "error: requires sender's email address")
 
 if not reciever_email_address:
     reciever_email_address = input("Reciever's email address > ")
 
+    if not reciever_email_address:
+        print(sys.argv[0], "error: requires sender's email address")
+
+
 if not message:
     message = input("Message > ")
 
+# Starting mail service
+mail = smtplib.SMTP('smtp.gmail.com', 587) 
+mail.ehlo() 
+mail.starttls()
 
-mail.login(sender_email_address, pw)
+pw = getpass.getpass()
 
+mail.login(sender_email_address, pw) 
 mail.sendmail(sender_email_address, reciever_email_address, message) 
-
 mail.close()
 
